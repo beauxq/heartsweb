c6528fb9a978d3bd056b0b9c2e63386cf6228220
beauxq <beauxq@users.noreply.github.com>
2018-07-20 10:49:10 -0500

    Game class

---
+++ Game.ts
@@ -0,0 +1,51 @@
+import GameHand from "./GameHand";
+
+class Game {
+    static passingDirections: number[] = [1, 3, 2, 0];  // left, right, across, keep
+
+    public hand: GameHand = new GameHand();
+    public scores: number[] = [0, 0, 0, 0];
+    public winners: number[] = [];  // empty if game is not finished
+    private passingIndex: number = 0;
+
+    public reset() {
+        this.winners.length = 0;
+        this.passingIndex = 0;
+        this.scores[0] = 0;
+        this.scores[1] = 0;
+        this.scores[2] = 0;
+        this.scores[3] = 0;
+    }
+
+    public endHand() {
+        let gameOver = false;
+        for (let player = 0; player < 4; ++player) {
+            this.scores[player] += this.hand.getScore(player);
+            if (this.scores[player] > 99) {
+                gameOver = true;
+            }
+        }
+        if (gameOver) {
+            this.winners.push(0);
+            for (let player = 1; player < 4; ++player) {
+                if (this.scores[player] < this.scores[this.winners[0]]) {  // better
+                    this.winners.length = 0;
+                    this.winners.push(player);
+                }
+                else if (this.scores[player] === this.scores[this.winners[0]]) {  // tie
+                    this.winners.push(player);
+                }
+            }
+        }
+    }
+
+    public changePassing() {
+        this.passingIndex = (this.passingIndex + 1) % 4;
+    }
+
+    public getPassingDirection() {
+        return Game.passingDirections[this.passingIndex];
+    }
+}
+
+export default Game;
--- Gui.ts
+++ Gui.ts
@@ -1,10 +1,30 @@
-class Gui {
+import HandObserver from "./HandObserver";
+import Card from "./Card";
+import Game from "./Game";
+import AI from "./AI";
+
+class Gui implements HandObserver {
     private context: CanvasRenderingContext2D;
     private assets: HTMLElement;
+    private game: Game;
+    private ais: (AI|null)[];
 
     constructor(context: CanvasRenderingContext2D, assets: HTMLElement) {
         this.context = context;
         this.assets = assets;
+
+        this.game = new Game();
+        this.ais = [
+            null,  // unused
+            new AI(this.game.hand, 1),
+            new AI(this.game.hand, 2),
+            new AI(this.game.hand, 3)
+        ];
+
+        this.game.hand.registerObserver(this);
+        for (let player = 1; player < 4; ++player) {
+            this.game.hand.registerObserver(this.ais[player] as AI);
+        }
     }
 
     draw() {
@@ -13,6 +33,25 @@ class Gui {
 
     click(e: MouseEvent) {
         console.log("gui click: ", e.x, e.y);
+    }
+
+    resetHand(): void {
+        this.draw();
+    }
+    dealHands(): void {
+        this.draw();
+    }
+    receivePassedCards(): void {
+        this.draw();
+    }
+    resetTrick(): void {
+        this.draw();
+    }
+    pass(fromPlayer: number, toPlayer: number, cards: Card[]) {
+        this.draw();
+    }
+    seeCardPlayed(card: Card, byPlayer: Number, showingOnlyHearts: boolean) {
+        this.draw();
     }
 }
 
