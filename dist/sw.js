96ce6f8f355c1f9456be2188868dece56149c735
beauxq <beauxq@users.noreply.github.com>
2020-10-20 09:30:53 -0700

    clean comments in sw

--- dist/sw.js
+++ dist/sw.js
@@ -22,44 +22,45 @@ this.addEventListener('install', async function() {
 /**
  * The fetch event is fired every time the browser sends a request. 
  * In this case, the service worker acts as a proxy. We can for example return the cached
- * version of the ressource matching the request, or send the request to the internet
- * , we can even make our own response from scratch !
+ * version of the resource matching the request, or send the request to the internet.
+ * We can even make our own response from scratch !
  * Here, we are going to use cache first strategy
  */
 self.addEventListener('fetch', event => {
-    //We defind the promise (the async code block) that return either the cached response or the network one
-    //It should return a response object
+    // We define the promise (the async code block) that return
+    // either the cached response or the network one
+    // It should return a response object.
     async function getCustomResponsePromise() {
         console.log(`URL ${event.request.url}`, `location origin ${location}`)
 
         try {
-            //Try to get the cached response
+            // Try to get the cached response
             const cachedResponse = await caches.match(event.request)
             if (cachedResponse) {
-                //Return the cached response if present
+                // Return the cached response if present
                 console.log(`Cached response ${cachedResponse}`)
                 return cachedResponse
             }
-
-            //Get the network response if no cached response is present
+            // else no cache
+            // get the network response
             const netResponse = await fetch(event.request)
+
             console.log(`adding net response to cache`)
-
-            //Here, we add the network response to the cache
+            // open the cache to put the network response in
             let cache = await caches.open(CACHE_NAME)
-
-            //We must provide a clone of the response here
+            // We must provide a clone of the response here
             cache.put(event.request, netResponse.clone())
 
-            //return the network response
             return netResponse
-        } catch (err) {
+        }
+        catch (err) {
             console.error(`Error ${err}`)
             throw err
         }
     }
 
-    //In order to override the default fetch behavior, we must provide the result of our custom behavoir to the
-    //event.respondWith method
+    // In order to override the default fetch behavior,
+    // we must provide the result of our custom behavior to the
+    // event.respondWith method
     event.respondWith(getCustomResponsePromise())
 });
