5c7185f36b207a62c248da876d642cead9b7e2b9
beauxq <beauxq@users.noreply.github.com>
2020-11-01 18:29:55 -0800

    workaround for landscape to portrait bug

--- src/hearts.ts
+++ src/hearts.ts
@@ -14,6 +14,7 @@ canvas.addEventListener('click', click, false);
 
 function resizeCanvas() {
     console.log(`browserZoomLevel: ${window.devicePixelRatio}`);
+    // ^ always 3 on my phone (including with l2p bug)
 
     canvas.width = window.innerWidth;
     // I've looked at a lot of google search results and still can't find a good solution to this problem
@@ -24,6 +25,39 @@ function resizeCanvas() {
     gui.resize(Math.min(window.devicePixelRatio, 1));  // cards don't fit on the screen if zoom past 100%
 
     gui.draw();
+}
+
+// I had an issue where
+// switching from landscape to portrait
+// would have a different window size from
+// loading the page in portrait
+
+// I found someone else with a similar issue and their solution worked.
+// I don't really understand it all.
+// I suspect it has something to do with the canvas size in landscape
+// causing overflow width in portrait.
+// (But then why wouldn't the canvas size in portrait cause overflow height in landscape?)
+// (And it doesn't look like the person asking the question in stackoverflow is using a canvas. Maybe it's not the same issue. But the solution works.)
+
+// https://stackoverflow.com/questions/53487190/why-is-chrome-shrinking-the-view-in-responsive-mode
+window.onorientationchange = function() { 
+    const htmlElement =  document.getElementsByTagName("html")[0];
+    const bodyElement = document.getElementsByTagName("body")[0];
+
+    if(window.innerWidth < window.innerHeight) {
+        // landscape to portrait
+        htmlElement.style.overflowX = "hidden";
+        bodyElement.style.overflowX = "hidden";
+     }
+     else {
+        // portrait to landscape
+        // I took these 2 lines from the stackoverflow solution and put them in the html file
+        // htmlElement.css("overflow","auto");
+        // bodyElement.css("overflow", "auto");
+        // below 2 lines makes the UI not shrink in portrait mode 
+        htmlElement.style.overflowX = "auto";
+        bodyElement.style.overflowX = "auto";
+     }
 }
 
 function click(e: MouseEvent) {
